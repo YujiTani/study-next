6d2f98134723ce0251f215b80967b626a1d73dcd
Yuji Tani <tani@leanonme.co.jp>
2022-07-01 00:18:35 +0900

    コンポーネント毎にCSS moduleを使用するように変更

---
+++ pages/components/Main.jsx
@@ -0,0 +1,18 @@
+import Links from './Links';
+import classes from './Main.module.css';
+
+export default function Main() {
+  return (
+    <main className={classes.main}>
+      <h1 className={classes.title}>
+        Welcome to <a href='https://nextjs.org'>Next.js!</a>
+      </h1>
+
+      <p className={classes.description}>
+        Get started by editing{' '}
+        <code className={classes.code}>pages/index.js</code>
+      </p>
+      <Links />
+    </main>
+  );
+}
