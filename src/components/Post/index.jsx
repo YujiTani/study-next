739780e61d92469a373733f7372f696903aec84d
Yuji Tani <tani@leanonme.co.jp>
2022-07-04 00:46:02 +0900

    useReducerを使う処理に変更、ローディング処理などを追加

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,31 +1,74 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useReducer } from 'react';
 import classes from './Post.module.css';
 
 export default function Post() {
-  const [posts, setPosts] = useState([]);
+  const initialArg = {
+    posts: [],
+    fetching: true,
+    error: null,
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'end':
+        return {
+          ...state,
+          posts: action.posts,
+          fetching: false,
+        };
+      case 'error':
+        return {
+          ...state,
+          fetching: false,
+          error: action.error,
+        };
+      default:
+        throw new Error('No such action type!');
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialArg);
 
   const getPosts = useCallback(async () => {
-    const responce = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const responceJson = await responce.json();
-    setPosts(responceJson);
+    try {
+      const responce = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+      if (!responce.ok) {
+        throw new Error('データ取得に失敗しました。');
+      }
+      const responceJson = await responce.json();
+      await dispatch({ type: 'end', posts: responceJson });
+    } catch (error) {
+      dispatch({ type: 'error', error });
+    }
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
+  if (state.fetching) {
+    return <h2 style={{ textAlign: 'center' }}>データ取得中...</h2>;
+  }
+
+  if (state.error) {
+    return <h2 style={{ textAlign: 'center' }}>エラーが発生しました。</h2>;
+  }
+
+  if (state.posts.length === 0) {
+    return <h2 style={{ textAlign: 'center' }}>データがありません。</h2>;
+  }
+
   return (
     <ul>
-      {posts.length > 0
-        ? posts.map((post) => {
-            return (
-              <ol key={posts.id} className={classes.postWrapper}>
-                <p>{post.title}</p>
-                <p>{post.body}</p>
-              </ol>
-            );
-          })
-        : 'データがありません'}
+      {state.posts.map((post) => {
+        return (
+          <ol key={post.id} className={classes.postWrapper}>
+            <p>{post.title}</p>
+            <p>{post.body}</p>
+          </ol>
+        );
+      })}
     </ul>
   );
 }
